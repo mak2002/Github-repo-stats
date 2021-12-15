108c82bde21facd3091f7b53856321db9a0d2af2
mak2002 <makbond0902@gmail.com>
2021-12-15 21:53:41 +0530

    minor ui changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ function App() {
   const [url, seturl] = useState();
   const [owner, setowner] = useState();
   const [repo, setrepo] = useState();
-  const [branch, setbranch] = useState('main');
+  const [branch, setbranch] = useState("main");
   const [repoInfo, setrepoInfo] = useState();
   const [isReadme, setisReadme] = useState();
-  const [issues, setissues] = useState();
+  const [issues, setissues] = useState([]);
+
+  const [filteredIssues, setfilteredIssues] = useState([]);
+  const [filteredIssues1, setfilteredIssues1] = useState([]);
 
   const [lastCommit, setlastCommit] = useState();
   const [WeeklyCommits, setWeeklyCommits] = useState();
+
+  var tempArray = [];
 
   const handleClick = () => {
     var a = document.createElement("a");
@@ -26,64 +31,72 @@ function App() {
 
     fetchCommit(owner[0], repo[1]);
     fetchWeeklyCommits(owner[0], repo[1]);
-    fetchIssues(owner[0], repo[1])
+    fetchIssues(owner[0], repo[1]);
     fetchRepo(owner[0], repo[1]);
-    fetchReadme(owner[0], repo[1])
+    fetchReadme(owner[0], repo[1]);
 
-    console.log('issues : ', issues);
+    console.log("issues : ", filteredIssues.length);
   };
 
   const fetchCommit = (owner, repo) => {
     fetch(`https://api.github.com/repos/${owner}/${repo}/commits/${branch}`)
-    .then((data) => data.json())
-    .then((data) => setlastCommit(data.commit.author.date));
-    
+      .then((data) => data.json())
+      .then((data) => setlastCommit(data.commit.author.date));
   };
 
   const fetchWeeklyCommits = (owner, repo) => {
     fetch(`https://api.github.com/repos/${owner}/${repo}/stats/participation`)
-    .then((data) => data.json())
-    .then((data) => setWeeklyCommits(data.all[data.all.length - 1]))
-  }
+      .then((data) => data.json())
+      .then((data) => setWeeklyCommits(data.all[data.all.length - 1]));
+  };
 
+  var count = 0;
   const filterIssue = (issues) => {
     issues.map((issue) => {
-      if (!issue.hasOwnProperty('pull_request')) {
-        console.log('issue pull request : ',issue)
-        setissues([...issues, issue])
+      if (!issue.hasOwnProperty("pull_request")) {
+        console.log("issue pull request : ", issue);
+        tempArray.push(issue);
+        setfilteredIssues(tempArray);
       }
-    })
-  }
+    });
+  };
 
   const fetchIssues = (owner, repo) => {
-    fetch(`https://api.github.com/repos/${owner}/${repo}/issues`)
-    .then((data) => data.json())
-    // .then((data) => setissues(data))
-    // .then((data) => console.log(data))
-    .then((data) => filterIssue(data))
+    fetch(
+      `https://api.github.com/repos/${owner}/${repo}/issues?labels=$good first issue`
+    )
+      .then((data) => data.json())
+      // .then((data) => setissues(data))
+      // .then((data) => console.log(data))
+      .then((data) => filterIssue(data));
+  };
 
-  }
+  const fetchIssues1 = (owner, repo) => {
+    fetch(`https://api.github.com/repos/${owner}/${repo}/issues?labels=easy`)
+      .then((data) => data.json())
+      .then((data) => filterIssue(data));
+  };
 
   const fetchRepo = (owner, repo) => {
     fetch(`https://api.github.com/repos/${owner}/${repo}`)
-    .then((data) => data.json())
-    .then((data) => setrepoInfo(data.created_at))
+      .then((data) => data.json())
+      .then((data) => setrepoInfo(data.created_at));
 
-    console.log()
-  }
+    console.log();
+  };
 
   const fetchReadme = (owner, repo) => {
-    fetch(`https://api.github.com/repos/${owner}/${repo}/readme`)
-    .then((data) => {
-      if(data.status === 404) {
-        setisReadme(false);
+    fetch(`https://api.github.com/repos/${owner}/${repo}/readme`).then(
+      (data) => {
+        if (data.status === 404) {
+          setisReadme(false);
+        } else {
+          setisReadme(true);
+        }
       }
-      else{
-        setisReadme(true);
-      }
-    })
-  }
- 
+    );
+  };
+
   return (
     <div className="App bg-gray-500 h-screen">
       <div className="content-wrapper p-5 h-screen">
@@ -98,19 +111,56 @@ function App() {
           placeholder="Enter Github Repo URL"
         />
 
-        <select className="p-2.5 font-xl" name="" id="" onChange={(e) => setbranch(e.target.value)}>
-          <option value="main" font-2lg="true">main</option>
-          <option value="master" font-2lg="true">master</option>
+        <select
+          className="p-2.5 font-xl"
+          name=""
+          id=""
+          onChange={(e) => setbranch(e.target.value)}
+        >
+          <option value="main" font-2lg="true">
+            main
+          </option>
+          <option value="master" font-2lg="true">
+            master
+          </option>
+          <option value="gh-pages" font-2lg="true">
+            gh-pages
+          </option>
+          <option value="dev" font-2lg="true">
+            dev
+          </option>
         </select>
 
         <button className="bg-blue-600 p-2 " onClick={handleClick}>
           Get Stats
         </button>
 
-        {lastCommit && <h3>Last Commit: {lastCommit}</h3>}
-        {WeeklyCommits && <h2>Weekly Commit: {WeeklyCommits}</h2>}
-        {repoInfo && <h2>Repository Created at : {repoInfo}</h2>}
-        {isReadme ? <h2>Readme Available</h2> : <h2>Readme Not Available</h2>}
+        <div className="repo-info flex-col justify-start">
+          {lastCommit && (
+            <p className="text-2xl font-bold">Last Commit: {lastCommit}</p>
+          )}
+          {WeeklyCommits ? (
+            <p className="text-2xl font-bold">
+              Weekly Commit Activity: {WeeklyCommits}
+            </p>
+          ) : (
+            <p className="text-2xl font-bold">Weekly Commit Activity: 0</p>
+          )}
+
+          {repoInfo && (
+            <p className="text-2xl font-bold">
+              Repository Created at : {repoInfo}
+            </p>
+          )}
+          {isReadme ? (
+            <h1 className="text-2xl font-bold">Readme Available &#128077;</h1>
+          ) : null}
+          {filteredIssues ? (
+            filteredIssues.map((issue) => <p>Issue: {issue.title}</p>)
+          ) : (
+            <p className="text-2xl font-bold">No Issue</p>
+          )}
+        </div>
       </div>
     </div>
   );
